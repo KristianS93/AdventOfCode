693bb5279dcd3661a075a3cb8a685d89b6e2df90
Kristian <KristianS93@hotmail.com>
2022-12-02 23:10:36 +0100

    xD

--- 2022/Rust/target/debug/day2.d
+++ 2022/Rust/target/debug/day2.d
@@ -1,1 +1,1 @@
-/Users/kks/Documents/GitRepos/AdventOfCode/2022/Rust/AOC/target/debug/day2: /Users/kks/Documents/GitRepos/AdventOfCode/2022/Rust/AOC/src/bin/day2.rs /Users/kks/Documents/GitRepos/AdventOfCode/2022/Rust/AOC/src/bin/inputDay2.txt
+/Users/kks/Documents/GitRepos/AdventOfCode/2022/Rust/target/debug/day2: /Users/kks/Documents/GitRepos/AdventOfCode/2022/Rust/src/bin/day2.rs /Users/kks/Documents/GitRepos/AdventOfCode/2022/Rust/src/bin/inputDay2.txt

97c2c6cd53330bfdc8bd172895099c6b1c998dce
Kristian Desktop <KristianS93@hotmail.com>
2022-12-04 16:36:39 +0100

    day 5

---
+++ 2022/Rust/target/debug/day5.d
@@ -0,0 +1,1 @@
+C:\Users\krist\Documents\GitRepos\AdventOfCode\2022\Rust\target\debug\day5.exe: C:\Users\krist\Documents\GitRepos\AdventOfCode\2022\Rust\src\bin\day5.rs
---
+++ 2022/Rust/target/debug/deps/day5.d
@@ -0,0 +1,5 @@
+C:\Users\krist\Documents\GitRepos\AdventOfCode\2022\Rust\target\debug\deps\day5.exe: src\bin\day5.rs
+
+C:\Users\krist\Documents\GitRepos\AdventOfCode\2022\Rust\target\debug\deps\day5.d: src\bin\day5.rs
+
+src\bin\day5.rs:

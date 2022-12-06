72cc5938d43d312062403c9c8298a6d763cb6915
Kristian Desktop <KristianS93@hotmail.com>
2022-12-07 00:41:29 +0100

    finished day 5 part 1

--- 2022/Rust/src/bin/day5.rs
+++ 2022/Rust/src/bin/day5.rs
@@ -1,78 +1,51 @@
-struct Results {
-    column_1
-}
-let mut COLUMN_1: Vec<&str> = Vec::new();
-let mut column_2: Vec<&str> = Vec::new();
-let mut column_3: Vec<&str> = Vec::new();
-
 fn main() {
-    let now = std::time::Instant::now();
-
-    //test
-
-
-    //adding initial values
-    column_1.push("Z");
-    column_1.push("N");
-
-    column_2.push("M");
-    column_2.push("C");
-    column_2.push("D");
-
-    column_3.push("P");
-
     let input: &str = include_str!("./inputDay5.txt");
 
-    //move x from y to x
-    // split "move "
-    //1 from 2 to 1
-    // split " from "
-    //1 2 to 1
+    //test vectors
 
-    // let mut temp_part1: Vec<i32> = Vec::new();
+    let column_1 = vec!["F", "T", "C", "L", "R", "P", "G", "Q"];
+    let column_2 = vec!["N", "Q", "H", "W", "R", "F", "S", "J"];
+    let column_3 = vec!["F", "B", "H", "W", "P", "M", "Q"];
+    let column_4 = vec!["V", "S", "T", "D", "F"];
+    let column_5 = vec!["Q", "L", "D", "W", "V", "F", "Z"];
+    let column_6 = vec!["Z", "C", "L", "S"];
+    let column_7 = vec!["Z", "B", "M", "V", "D", "F"];
+    let column_8 = vec!["T", "J", "B"];
+    let column_9 = vec!["Q", "N", "B", "G", "L", "S", "P", "H"];
 
-    let values: Vec<(i32, i32, i32)> = input
-        .split("\n")
-        .map(|line| line.trim().split_once("move ").unwrap())
-        .map(|(part1, part2)| {
-            // println!("{}", part2);
-            let (num1, x) = part2.split_once(" from ").unwrap();
-            // println!("{}", num1);
-            let (num2, num3) = x.split_once(" to ").unwrap();
-            // println!("{}", num2);
-            // println!("{}", num3);
-            (
-                num1.parse::<i32>().unwrap(),
-                num2.parse::<i32>().unwrap(),
-                num3.parse::<i32>().unwrap(),
-            )
-        })
-        .collect();
+    let mut columns = vec![
+        column_1, column_2, column_3, column_4, column_5, column_6, column_7, column_8, column_9,
+    ];
 
-    for x in values {
-        println!("{}, {}, {}", x.0, x.1, x.2);
-    }
-    println!("Time elapsed: {:.2?}", now.elapsed());
-}
+    let xxx = input.split('\n').map(|line| {
+        line.split_whitespace()
+            .filter_map(|s| s.parse::<usize>().ok())
+            .collect::<Vec<usize>>()
+    });
 
-//index 1 = move number (ie move 2, means move 2 entries)
-//index 2 = from column 
-//index 3 = to column
-fn match_moves(val: Vec<(i32, i32, i32)>){
+    // column_1.pop()
+    // columns[1].pop()
 
-    let mut numerator = 0;
-    for entry in val{
-
-    }
-
-}
-
-fn matching(from: i32, to: i32){
-    match from {
-        1 => {
-            let temp = column_1.pop();
-            
+    for inst in xxx {
+        //move 1 from 2 to 1
+        for _ in 0..inst[0] {
+            let from = inst[1] - 1;
+            let to = inst[2] - 1;
+            let temp = columns[from].pop().unwrap();
+            columns[to].push(temp);
         }
-        _ => unreachable!(),
     }
+
+    println!(
+        "{}{}{}{}{}{}{}{}{}",
+        columns[0].pop().unwrap(),
+        columns[1].pop().unwrap(),
+        columns[2].pop().unwrap(),
+        columns[3].pop().unwrap(),
+        columns[4].pop().unwrap(),
+        columns[5].pop().unwrap(),
+        columns[6].pop().unwrap(),
+        columns[7].pop().unwrap(),
+        columns[8].pop().unwrap()
+    );
 }
